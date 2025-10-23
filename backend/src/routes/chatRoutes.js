95d891670a922de32e2edf8c129d02ffa692635a
Susmita <susmitahubale@gmail.com>
2025-10-23 07:37:56 +0530

    Initial commit

--- backend/src/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { processQuery } = require('../controllers/chatController');
-
-// Public routes
-router.post('/query', processQuery);
-
-module.exports = router;
